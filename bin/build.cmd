68f49a06ee0ad2d8c0f38c1cd5ed06d50679fc52
Jason Belt <belt@ksu.edu>
2020-02-24 18:15:40 -0600

    add build

---
+++ bin/build.cmd
@@ -0,0 +1,196 @@
+::#! 2> /dev/null                                                                                           #
+@ 2>/dev/null # 2>nul & echo off & goto BOF                                                                 #
+export SIREUM_HOME=$(cd -P $(dirname "$0")/.. && pwd -P)                                                    #
+if [ ! -z ${SIREUM_PROVIDED_SCALA++} ]; then                                                                #
+  SIREUM_PROVIDED_JAVA=true                                                                                 #
+fi                                                                                                          #
+"${SIREUM_HOME}/bin/init.sh"                                                                                #
+if [ -n "$COMSPEC" -a -x "$COMSPEC" ]; then                                                                 #
+  export SIREUM_HOME=$(cygpath -C OEM -w -a ${SIREUM_HOME})                                                 #
+  if [ -z ${SIREUM_PROVIDED_JAVA++} ]; then                                                                 #
+    export PATH="${SIREUM_HOME}/bin/win/java":"${SIREUM_HOME}/bin/win/z3":$PATH                             #
+    export PATH="$(cygpath -C OEM -w -a ${JAVA_HOME}/bin)":"$(cygpath -C OEM -w -a ${Z3_HOME}/bin)":$PATH   #
+  fi                                                                                                        #
+elif [ "$(uname)" = "Darwin" ]; then                                                                        #
+  if [ -z ${SIREUM_PROVIDED_JAVA++} ]; then                                                                 #
+    export PATH="${SIREUM_HOME}/bin/mac/java/bin":"${SIREUM_HOME}/bin/mac/z3/bin":$PATH                     #
+  fi                                                                                                        #
+elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then                                                   #
+  if [ -z ${SIREUM_PROVIDED_JAVA++} ]; then                                                                 #
+    export PATH="${SIREUM_HOME}/bin/linux/java/bin":"${SIREUM_HOME}/bin/linux/z3/bin":$PATH                 #
+  fi                                                                                                        #
+fi                                                                                                          #
+if [ -f "$0.com" ] && [ "$0.com" -nt "$0" ]; then                                                           #
+  exec "$0.com" "$@"                                                                                        #
+else                                                                                                        #
+  rm -fR "$0.com"                                                                                           #
+  exec "${SIREUM_HOME}/bin/sireum" slang run -s -n "$0" "$@"                                                #
+fi                                                                                                          #
+:BOF
+setlocal
+call "%~dp0init.bat"
+set NEWER=False
+if exist %~dpnx0.com for /f %%i in ('powershell -noprofile -executionpolicy bypass -command "(Get-Item %~dpnx0.com).LastWriteTime -gt (Get-Item %~dpnx0).LastWriteTime"') do @set NEWER=%%i
+if "%NEWER%" == "True" goto native
+del "%~dpnx0.com" > nul 2>&1
+if defined SIREUM_PROVIDED_SCALA set SIREUM_PROVIDED_JAVA=true
+if not defined SIREUM_PROVIDED_JAVA set PATH=%~dp0win\java\bin;%~dp0win\z3\bin;%PATH%
+"%~dp0sireum.bat" slang run -s -n "%0" %*
+exit /B %errorlevel%
+:native
+%~dpnx0.com %*
+exit /B %errorlevel%
+::!#
+// #Sireum
+import org.sireum._
+
+
+def usage(): Unit = {
+  println("Sireum HAMR AADL Runtime Services /build")
+  println("Usage: ( compile | test | test-js | m2 | jitpack )+")
+}
+
+
+if (Os.cliArgs.isEmpty) {
+  usage()
+  Os.exit(0)
+}
+
+
+val homeBin = Os.slashDir
+val home = homeBin.up
+val sireumJar = homeBin / "sireum.jar"
+val mill = homeBin / "mill.bat"
+var didTipe = F
+var didCompile = F
+var didM2 = F
+
+
+def downloadMill(): Unit = {
+  if (!mill.exists) {
+    println("Downloading mill ...")
+    mill.downloadFrom("http://files.sireum.org/mill-standalone")
+    mill.chmod("+x")
+    println()
+  }
+}
+
+
+def tipe(): Unit = {
+  if (!didTipe) {
+    didTipe = T
+    println("Slang type checking ...")
+    Os.proc(ISZ("java", "-jar", sireumJar.string,
+      "slang", "tipe", "--verbose", "-r", "-s", home.string)).at(home).console.runCheck()
+    println()
+  }
+}
+
+
+def compile(): Unit = {
+  if (!didCompile) {
+    didCompile = T
+    if (didM2) {
+      didM2 = F
+      (home / "out").removeAll()
+    }
+    tipe()
+    println("Compiling ...")
+    mill.call(ISZ("all", "hamr.codegen.art.jvm.tests.compile",
+      "hamr.codegen.art.js.tests.compile")).at(home).console.runCheck()
+    println()
+  }
+}
+
+
+def test(): Unit = {
+  compile()
+  println("Running shared tests ...")
+  mill.call(ISZ("hamr.codegen.art.shared.tests")).at(home).console.runCheck()
+  println()
+
+  println("Running jvm tests ...")
+  mill.call(ISZ("hamr.codegen.art.jvm.tests")).at(home).console.runCheck()
+  println()
+}
+
+
+def testJs(): Unit = {
+  println("Running js tests ...")
+  mill.call(ISZ("hamr.codegen.art.js.tests")).at(home).console.runCheck()
+  println()
+}
+
+/*
+def jitpack(): Unit = {
+  println("Triggering jitpack ...")
+  val r = mill.call(ISZ("jitPack", "--owner", "sireum", "--repo", "slang-embedded-art", "--lib", "library")).
+    at(home).console.run()
+  r match {
+    case r: Os.Proc.Result.Normal =>
+      println(r.out)
+      println(r.err)
+      if (!r.ok) {
+        eprintln(s"Exit code: ${r.exitCode}")
+      }
+    case r: Os.Proc.Result.Exception =>
+      eprintln(s"Exception: ${r.err}")
+    case _: Os.Proc.Result.Timeout =>
+      eprintln("Timeout")
+      eprintln()
+  }
+  println()
+}
+*/
+
+def m2(): Unit = {
+  didM2 = T
+  didCompile = F
+
+  val m2s: ISZ[ISZ[String]] =
+    for (pkg <- ISZ("art"); plat <- ISZ("shared", "jvm"))
+      yield ISZ(pkg, plat, "m2")
+
+  val m2Paths: ISZ[Os.Path] =
+    for (cd <- for (m2 <- m2s) yield st"${(m2, Os.fileSep)}".render) yield  home / "out" / cd
+
+  for (m2p <- m2Paths) {
+    m2p.removeAll()
+  }
+
+  (home / "out").removeAll()
+
+  Os.proc(ISZ[String](mill.string, "all") ++ (for (m2 <- m2s) yield st"${(m2, ".")}".render)).
+    at(home).env(ISZ("SIREUM_SOURCE_BUILD" ~> "false")).console.runCheck()
+
+  val repository = Os.home / ".m2" / "repository"
+
+  println()
+  println("Artifacts")
+  for (m2p <- m2Paths; p <- (m2p / "dest").overlayMove(repository, F, F, _ => T, T).values) {
+    println(s"* $p")
+  }
+  println()
+}
+
+def cloneRuntime(): Unit ={
+  (home / "runtime").removeAll()
+  Os.proc(ISZ[String]("git", "clone", "--depth=1", "https://github.com/sireum/runtime")).at(home).console.runCheck()
+}
+
+
+downloadMill()
+cloneRuntime()
+for (i <- 0 until Os.cliArgs.size) {
+  Os.cliArgs(i) match {
+    case string"compile" => compile()
+    case string"test" => test()
+    case string"test-js" => testJs()
+    case string"m2" => m2()
+    case string"jitpack" => jitpack()
+    case cmd =>
+      usage()
+      eprintln(s"Unrecognized command: $cmd")
+      Os.exit(-1)
+  }
+}

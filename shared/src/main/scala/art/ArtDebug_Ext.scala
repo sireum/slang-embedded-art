540f78f4f43b8e025c462654ebaa8f936c747e1c
Matthew Weis <matthewweiscaps@gmail.com>
2020-02-28 16:56:24 -0600

    inspector integration

--- shared/src/main/scala/art/ArtDebug_Ext.scala
+++ shared/src/main/scala/art/ArtDebug_Ext.scala
@@ -1,31 +1,25 @@
 package art
 
 import org.sireum._
+import art.Art.Time
 import scala.collection.mutable.{Map => MMap, Set => MSet}
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import scala.util.Failure
 
 object ArtDebug_Ext {
   private val debugObjects: MMap[String, Any] = ArtNative_Ext.concMap()
-  private val listeners: MMap[Art.PortId, (Bridge, UPort, MSet[Listener])] = ArtNative_Ext.concMap()
+  private val listeners: MSet[ArtListener] = concSet()
 
-  /**
-    * Set to false to disable futures on listenerCallbacks. If false then avoid blocking the thread.
-    */
-  var forkListenerCallbacks: Boolean = true
+  protected[art] def start(): Unit = {
+    val time = Art.time()
+    listeners.foreach((listener: ArtListener) => listener.start(time))
+  }
 
-  protected[art] def portListenerCallback(p: Art.PortId, d: DataContent): Unit = {
-    if (forkListenerCallbacks) {
-      listeners.get(p).map(e => e._3.foreach( c =>
-        Future ( c.callback(e._1, e._2, d) ) onComplete {
-          case Failure(e) => throw e
-          case _ =>
-        }
-      ))
-    } else {
-      listeners.get(p).map(e => e._3.foreach(_.callback(e._1, e._2, d)))
-    }
+  protected[art] def stop(): Unit = {
+    val time = Art.time()
+    listeners.foreach((listener: ArtListener) => listener.stop(time))
+  }
+
+  protected[art] def outputCallback(src: Art.PortId, dst: Art.PortId, data: DataContent, time: Time): Unit = {
+    listeners.foreach((listener: ArtListener) => listener.output(src, dst, data, time))
   }
 
   def setDebugObject[T](key: String, o: T): Unit = {
@@ -64,43 +58,8 @@ object ArtDebug_Ext {
     }
   }
 
-  def registerPortListener(portId: Art.PortId, listener: PortListener): Unit = {
-    val bridge = Art.bridges.elements.filter(_.nonEmpty).map(_.get).filter(b => b.ports.all.elements.map(_.id).contains(portId)).head
-    registerListener(bridge, Seq(Art.port(portId)), listener)
-  }
-
-  def registerBridgeListener(bridgeId:Art.BridgeId, listener: BridgeListener): Unit = {
-    val bridge = Art.bridge(bridgeId)
-    val ports = concSet[UPort]()
-
-    if(listener.isInstanceOf[BridgeInDataListener])
-      ports ++= bridge.ports.dataIns.elements
-    if(listener.isInstanceOf[BridgeInEventListener])
-      ports ++= bridge.ports.eventIns.elements
-
-    if(listener.isInstanceOf[BridgeOutDataListener])
-      ports ++= bridge.ports.dataOuts.elements
-    if(listener.isInstanceOf[BridgeOutEventListener])
-      ports ++= bridge.ports.eventOuts.elements
-
-    if(!(listener.isInstanceOf[BridgeInDataListener] || listener.isInstanceOf[BridgeInEventListener] ||
-        listener.isInstanceOf[BridgeOutDataListener] || listener.isInstanceOf[BridgeOutEventListener])) {
-      ports ++= bridge.ports.all.elements
-    }
-    registerListener(bridge, ports.toSeq, listener)
-  }
-
-  def registerProjectListener(listener: Listener): Unit = {
-    art.Art.bridges.elements.filter(_.nonEmpty).foreach(b => registerListener(b.get, b.get.ports.all.elements, listener))
-  }
-
-  private def registerListener(bridge: Bridge, ports: Seq[UPort], listener: Listener): Unit = {
-    for (port <- ports) {
-      val c = if (port.mode == PortMode.DataIn || port.mode == PortMode.EventIn) "receives" else "sends"
-      val t = if (port.mode == PortMode.DataIn || port.mode == PortMode.DataOut) "data" else "event"
-      ArtNative.logDebug(Art.logTitle, s"Registered callback.  Triggered when port ${port.name} $c $t")
-      listeners.getOrElseUpdate(port.id, (bridge, port, concSet()))._3.add(listener)
-    }
+  def registerListener(listener: ArtListener): Unit = {
+    listeners.add(listener)
   }
 
   def concSet[K](): MSet[K] = {

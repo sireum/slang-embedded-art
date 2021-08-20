7f8abdc968e06a73ff79f5eeaa66e5b092eb3dd4
Robby <robby@santoslab.org>
2021-08-20 07:45:48 -0500

    Updated mill download URL.

--- bin/build.cmd
+++ bin/build.cmd
@@ -71,7 +71,7 @@ val project: Os.Path = homeBin / "project4testing.cmd"
 def downloadMill(): Unit = {
   if (!mill.exists) {
     println("Downloading mill ...")
-    mill.downloadFrom("https://github.com/sireum/releases/releases/download/mill/mill")
+    mill.downloadFrom("https://github.com/sireum/rolling/releases/download/mill/standalone")
     mill.chmod("+x")
     println()
   }
